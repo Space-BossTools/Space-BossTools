4fd6a5795803396b472c48f1de0e11726585b9f5
MrScautHD <77861350+Luli252@users.noreply.github.com>
2021-03-18 14:58:01 +0100

    Fix Fall back to Earth Teleport

--- src/main/java/net/mrscauthd/boss_tools/procedures/UmlaufbahneffectsProcedure.java
+++ src/main/java/net/mrscauthd/boss_tools/procedures/UmlaufbahneffectsProcedure.java
@@ -11,15 +11,16 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.util.math.vector.Vector2f;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.network.play.server.SPlayerAbilitiesPacket;
+import net.minecraft.network.play.server.SPlayEntityEffectPacket;
+import net.minecraft.network.play.server.SChangeGameStatePacket;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.Entity;
-import net.minecraft.command.ICommandSource;
-import net.minecraft.command.CommandSource;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -66,12 +67,24 @@ public class UmlaufbahneffectsProcedure extends BossToolsModElements.ModElement 
 				&& world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z)))
 						.equals(new ResourceLocation("boss_tools:orbit_overworld_biom")))) {
 			if ((!((entity.getRidingEntity()) instanceof LandingGearEntity.CustomEntity))) {
-				if (((entity.getPosY()) < 1)) {
-					if (world instanceof ServerWorld) {
-						((World) world).getServer().getCommandManager().handleCommand(
-								new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
-										new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
-								"/execute in minecraft:overworld run teleport @p ~ 450 ~");
+				if (((entity.getPosY()) <= 1)) {
+					{
+						Entity _ent = entity;
+						if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+							RegistryKey<World> destinationType = World.OVERWORLD;
+							ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+							if (nextWorld != null) {
+								((ServerPlayerEntity) _ent).connection
+										.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241768_e_, 0));
+								((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), 450,
+										nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+								((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+								for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+									((ServerPlayerEntity) _ent).connection
+											.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+								}
+							}
+						}
 					}
 				}
 			}
@@ -80,12 +93,25 @@ public class UmlaufbahneffectsProcedure extends BossToolsModElements.ModElement 
 				&& world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z)))
 						.equals(new ResourceLocation("boss_tools:orbit_moon_biom")))) {
 			if ((!((entity.getRidingEntity()) instanceof LandingGearEntity.CustomEntity))) {
-				if (((entity.getPosY()) < 1)) {
-					if (world instanceof ServerWorld) {
-						((World) world).getServer().getCommandManager().handleCommand(
-								new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
-										new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
-								"/execute in boss_tools:moon run teleport @p ~ 450 ~");
+				if (((entity.getPosY()) <= 1)) {
+					{
+						Entity _ent = entity;
+						if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+							RegistryKey<World> destinationType = RegistryKey.getOrCreateKey(Registry.WORLD_KEY,
+									new ResourceLocation("boss_tools:moon"));
+							ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+							if (nextWorld != null) {
+								((ServerPlayerEntity) _ent).connection
+										.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241768_e_, 0));
+								((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), 450,
+										nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+								((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+								for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+									((ServerPlayerEntity) _ent).connection
+											.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+								}
+							}
+						}
 					}
 				}
 			}
@@ -94,12 +120,25 @@ public class UmlaufbahneffectsProcedure extends BossToolsModElements.ModElement 
 				&& world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z)))
 						.equals(new ResourceLocation("boss_tools:orbit_mars_biom")))) {
 			if ((!((entity.getRidingEntity()) instanceof LandingGearEntity.CustomEntity))) {
-				if (((entity.getPosY()) < 1)) {
-					if (world instanceof ServerWorld) {
-						((World) world).getServer().getCommandManager().handleCommand(
-								new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
-										new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
-								"/execute in boss_tools:mars run teleport @p ~ 450 ~");
+				if (((entity.getPosY()) <= 1)) {
+					{
+						Entity _ent = entity;
+						if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+							RegistryKey<World> destinationType = RegistryKey.getOrCreateKey(Registry.WORLD_KEY,
+									new ResourceLocation("boss_tools:mars"));
+							ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+							if (nextWorld != null) {
+								((ServerPlayerEntity) _ent).connection
+										.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241768_e_, 0));
+								((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), 450,
+										nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+								((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+								for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+									((ServerPlayerEntity) _ent).connection
+											.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+								}
+							}
+						}
 					}
 				}
 			}
@@ -108,12 +147,25 @@ public class UmlaufbahneffectsProcedure extends BossToolsModElements.ModElement 
 				&& world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z)))
 						.equals(new ResourceLocation("boss_tools:orbit_mercury_biom")))) {
 			if ((!((entity.getRidingEntity()) instanceof LandingGearEntity.CustomEntity))) {
-				if (((entity.getPosY()) < 1)) {
-					if (world instanceof ServerWorld) {
-						((World) world).getServer().getCommandManager().handleCommand(
-								new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
-										new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
-								"/execute in boss_tools:mercury run teleport @p ~ 450 ~");
+				if (((entity.getPosY()) <= 1)) {
+					{
+						Entity _ent = entity;
+						if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+							RegistryKey<World> destinationType = RegistryKey.getOrCreateKey(Registry.WORLD_KEY,
+									new ResourceLocation("boss_tools:mercury"));
+							ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+							if (nextWorld != null) {
+								((ServerPlayerEntity) _ent).connection
+										.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241768_e_, 0));
+								((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), 450,
+										nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+								((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+								for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+									((ServerPlayerEntity) _ent).connection
+											.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+								}
+							}
+						}
 					}
 				}
 			}
