41385777d9ea2371b2a0e11794ae1fef4282402c
MrScautHD <77861350+Luli252@users.noreply.github.com>
2021-05-16 09:57:44 +0200

    Reworked Rocket Tank Animation

--- src/main/java/net/mrscauthd/boss_tools/gui/RocketTier1GUIFuelGuiWindow.java
+++ src/main/java/net/mrscauthd/boss_tools/gui/RocketTier1GUIFuelGuiWindow.java
@@ -3,30 +3,6 @@ package net.mrscauthd.boss_tools.gui;
 
 import org.lwjgl.opengl.GL11;
 
-import net.mrscauthd.boss_tools.procedures.RocketTank9Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank8Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank7Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank6Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank5Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank4Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank3Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank2Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank23Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank22Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank21Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank20Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank1Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank19Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank18Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank17Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank16Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank15Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank14Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank13Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank12Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank11Procedure;
-import net.mrscauthd.boss_tools.procedures.RocketTank10Procedure;
-
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
@@ -40,8 +16,6 @@ import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.Minecraft;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-
-import com.google.common.collect.ImmutableMap;
 
 @OnlyIn(Dist.CLIENT)
 public class RocketTier1GUIFuelGuiWindow extends ContainerScreen<RocketTier1GUIFuelGui.GuiContainerMod> {
@@ -76,97 +50,100 @@ public class RocketTier1GUIFuelGuiWindow extends ContainerScreen<RocketTier1GUIF
 		int k = (this.width - this.xSize) / 2;
 		int l = (this.height - this.ySize) / 2;
 		this.blit(ms, k, l, 0, 0, this.xSize, this.ySize, this.xSize, this.ySize);
+		// Tank
+		double tank = ((entity.getPersistentData().getDouble("fuel")));
+		// Textures
 		Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettank.png"));
 		this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
-		if (RocketTank1Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 17) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull1.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank2Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 37) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull2.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank3Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 51) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull3.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank4Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 68) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull4.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank5Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 85) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull5.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank6Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 102) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull6.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank7Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 119) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull7.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank8Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 136) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull8.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank9Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 153) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull9.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank10Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 170) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull10.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank11Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 187) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull11.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank12Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 204) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull12.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank13Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 221) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull13.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank14Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 238) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull14.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank15Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 255) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull15.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank16Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 272) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull16.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank17Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 289) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull17.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank18Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 306) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull18.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank19Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 323) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull19.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank20Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 340) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull20.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank21Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 357) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull21.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank22Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 374) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull22.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
-		if (RocketTank23Procedure.executeProcedure(ImmutableMap.of("entity", entity))) {
+		if (tank >= 400) {
 			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("boss_tools:textures/rockettankfull23.png"));
 			this.blit(ms, this.guiLeft + 66, this.guiTop + 21, 0, 0, 48, 48, 48, 48);
 		}
