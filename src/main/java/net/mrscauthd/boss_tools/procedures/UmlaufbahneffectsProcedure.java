e786a2b464df04f0a2244d96b5fac098d0e04577
MrScautHD <77861350+Luli252@users.noreply.github.com>
2021-06-13 08:07:13 +0200

    Fix Orbit Tps have checked Biomes and not Dimensions

--- src/main/java/net/mrscauthd/boss_tools/procedures/UmlaufbahneffectsProcedure.java
+++ src/main/java/net/mrscauthd/boss_tools/procedures/UmlaufbahneffectsProcedure.java
@@ -11,7 +11,6 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.potion.EffectInstance;
@@ -52,34 +51,15 @@ public class UmlaufbahneffectsProcedure {
 				BossToolsMod.LOGGER.warn("Failed to load dependency entity for procedure Umlaufbahneffects!");
 			return;
 		}
-		if (dependencies.get("x") == null) {
-			if (!dependencies.containsKey("x"))
-				BossToolsMod.LOGGER.warn("Failed to load dependency x for procedure Umlaufbahneffects!");
-			return;
-		}
-		if (dependencies.get("y") == null) {
-			if (!dependencies.containsKey("y"))
-				BossToolsMod.LOGGER.warn("Failed to load dependency y for procedure Umlaufbahneffects!");
-			return;
-		}
-		if (dependencies.get("z") == null) {
-			if (!dependencies.containsKey("z"))
-				BossToolsMod.LOGGER.warn("Failed to load dependency z for procedure Umlaufbahneffects!");
-			return;
-		}
 		if (dependencies.get("world") == null) {
 			if (!dependencies.containsKey("world"))
 				BossToolsMod.LOGGER.warn("Failed to load dependency world for procedure Umlaufbahneffects!");
 			return;
 		}
 		Entity entity = (Entity) dependencies.get("entity");
-		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
-		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
-		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		if ((world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z))) != null
-				&& world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z)))
-						.equals(new ResourceLocation("boss_tools:orbit_overworld_biom")))) {
+		if (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY,
+				new ResourceLocation("boss_tools:orbit_overworld"))))) {
 			if ((!((entity.getRidingEntity()) instanceof LandingGearEntity.CustomEntity))) {
 				if (((entity.getPosY()) <= 1)) {
 					{
@@ -103,9 +83,8 @@ public class UmlaufbahneffectsProcedure {
 				}
 			}
 		}
-		if ((world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z))) != null
-				&& world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z)))
-						.equals(new ResourceLocation("boss_tools:orbit_moon_biom")))) {
+		if (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY,
+				new ResourceLocation("boss_tools:orbit_moon"))))) {
 			if ((!((entity.getRidingEntity()) instanceof LandingGearEntity.CustomEntity))) {
 				if (((entity.getPosY()) <= 1)) {
 					{
@@ -130,9 +109,8 @@ public class UmlaufbahneffectsProcedure {
 				}
 			}
 		}
-		if ((world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z))) != null
-				&& world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z)))
-						.equals(new ResourceLocation("boss_tools:orbit_mars_biom")))) {
+		if (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY,
+				new ResourceLocation("boss_tools:orbit_mars"))))) {
 			if ((!((entity.getRidingEntity()) instanceof LandingGearEntity.CustomEntity))) {
 				if (((entity.getPosY()) <= 1)) {
 					{
@@ -157,9 +135,8 @@ public class UmlaufbahneffectsProcedure {
 				}
 			}
 		}
-		if ((world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z))) != null
-				&& world.func_241828_r().getRegistry(Registry.BIOME_KEY).getKey(world.getBiome(new BlockPos((int) x, (int) y, (int) z)))
-						.equals(new ResourceLocation("boss_tools:orbit_mercury_biom")))) {
+		if (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY,
+				new ResourceLocation("boss_tools:orbit_mercury"))))) {
 			if ((!((entity.getRidingEntity()) instanceof LandingGearEntity.CustomEntity))) {
 				if (((entity.getPosY()) <= 1)) {
 					{
