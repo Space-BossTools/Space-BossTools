8ced04fd72e8006d3e47cde9e61dc95233da951b
MrScautHD <77861350+Luli252@users.noreply.github.com>
2021-07-22 12:53:17 +0200

    SetWeater to Client Side

--- src/main/java/net/mrscauthd/boss_tools/procedures/SetPlanetWeatherProcedure.java
+++ src/main/java/net/mrscauthd/boss_tools/procedures/SetPlanetWeatherProcedure.java
@@ -1,6 +1,8 @@
 package net.mrscauthd.boss_tools.procedures;
 
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.TickEvent;
@@ -10,14 +12,16 @@ import net.minecraft.world.IWorld;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.RegistryKey;
+import org.apache.logging.log4j.core.jmx.Server;
 
 public class SetPlanetWeatherProcedure {
 	@Mod.EventBusSubscriber
 	private static class GlobalTrigger {
+		@OnlyIn(Dist.CLIENT)
 		@SubscribeEvent
 		public static void onWorldTick(TickEvent.WorldTickEvent event) {
 			if (event.phase == TickEvent.Phase.END) {
-				IWorld world = event.world;
+				World world = event.world;
 				if ((((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("boss_tools:moon")))) || (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("boss_tools:orbit_moon")))) || (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("boss_tools:mars")))) || (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("boss_tools:orbit_mars")))) || (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("boss_tools:mercury")))) || (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("boss_tools:orbit_mercury")))) || (((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("boss_tools:orbit_venus")))) || ((world instanceof World ? (((World) world).getDimensionKey()) : World.OVERWORLD) == (RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("boss_tools:orbit_overworld")))))))))))) {
 					if (world instanceof ServerWorld) {
 						((ServerWorld) world).setRainStrength(0);
