28eb0e3f85e1b5886797e38357ecca1ae4255f93
MrScautHD <77861350+Luli252@users.noreply.github.com>
2021-07-28 11:02:29 +0200

    test

--- src/main/java/net/mrscauthd/boss_tools/entity/MoglerEntity.java
+++ src/main/java/net/mrscauthd/boss_tools/entity/MoglerEntity.java
@@ -64,7 +64,7 @@ public class MoglerEntity extends BossToolsModElements.ModElement {
 
         public CustomEntity(EntityType<CustomEntity> type, World world) {
             super(type, world);
-            //experienceValue = 0;
+            //experienceValue = 0; x
             setNoAI(false);
         }
 
