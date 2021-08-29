fa5c4e3b77b81cd69c05ff83c90dcaa757ca5ac8
Luli252 <77861350+Luli252@users.noreply.github.com>
2021-08-29 13:12:57 +0200

    fix
    fix

--- src/main/java/net/mrscauthd/boss_tools/jei/JeiPlugin.java
+++ src/main/java/net/mrscauthd/boss_tools/jei/JeiPlugin.java
@@ -22,7 +22,6 @@ import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.IRecipeTransferRegistration;
-import mezz.jei.config.Constants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.Rectangle2d;
