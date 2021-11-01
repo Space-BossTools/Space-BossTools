473fe801a4af9f66f8151b0c4dd279eefea7d128
MrScautHD <MrScautHD5@gmail.com>
2021-11-01 10:04:49 +0100

    false Github merge
    false Github merge

--- src/main/java/net/mrscauthd/boss_tools/machines/OxygenBubbleDistributorBlock.java
+++ src/main/java/net/mrscauthd/boss_tools/machines/OxygenBubbleDistributorBlock.java
@@ -251,22 +251,11 @@ public class OxygenBubbleDistributorBlock {
 			double range = this.getRange();
 			int oxygenUsing = this.getOxygenUsing(range);
 
-			if (this.consumeIngredients() != true) {
-				if (this.consumePowerForOperation() == null) {
-					return;
-				}
-			}
-
 			if (oxygenStorage.extractOxygen(oxygenUsing, true) == oxygenUsing) {
-				oxygenStorage.extractOxygen(oxygenUsing, false);
-				if (oxygenStorage.extractOxygen(oxygenUsing, true) == oxygenUsing) {
-					if (this.isProcessedInThisTick() || this.consumePowerForOperation() != null) {
-						oxygenStorage.extractOxygen(oxygenUsing, false);
-						this.spawnOxygenBubble(range);
-					}
-
+				if (this.isProcessedInThisTick() || this.consumePowerForOperation() != null) {
+					oxygenStorage.extractOxygen(oxygenUsing, false);
+					this.spawnOxygenBubble(range);
 				}
-
 			}
 		}
 
