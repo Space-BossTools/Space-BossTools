9b3ad93f30306f7f6b6674df0b517b07ae18bccc
MrScautHD <MrScautHD5@gmail.com>
2021-11-09 14:01:26 +0100

    Added Water Pump Part 1 / 2
    Added Water Pump Part 1 / 2

--- src/main/java/net/mrscauthd/boss_tools/machines/tile/WaterPumpTileEntity.java
+++ src/main/java/net/mrscauthd/boss_tools/machines/tile/WaterPumpTileEntity.java
@@ -1,19 +1,35 @@
 package net.mrscauthd.boss_tools.machines.tile;
 
-import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.IBucketPickupHandler;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.inventory.container.Container;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
 import net.mrscauthd.boss_tools.ModInnet;
+import net.mrscauthd.boss_tools.fluid.FluidUtil2;
+import net.mrscauthd.boss_tools.machines.WaterPump;
+
+import java.util.function.Predicate;
 
 public class WaterPumpTileEntity extends AbstractMachineTileEntity {
     public WaterPumpTileEntity() {
         super(ModInnet.WATER_PUMP.get());
     }
 
+    public static final int TANK_CAPACITY = 6000;
     public double WATER_TIMER = 0;
+    public static final ResourceLocation WATER_TANK = new ResourceLocation("boss_tools", "water_tank");
+    private FluidTank waterTank;
 
     @Override
     protected Container createMenu(int id, PlayerInventory player) {
@@ -24,16 +40,22 @@ public class WaterPumpTileEntity extends AbstractMachineTileEntity {
     public void tick() {
         BlockPos pos = new BlockPos(this.pos.getX(),this.pos.getY() - 1, this.pos.getZ());
 
-        if (this.world.getBlockState(pos).getBlock() == Blocks.WATER) {
+        if (this.world.getFluidState(pos) == Fluids.WATER.getStillFluidState(false)) {
 
-            if (this.consumePowerForOperation() != null) {
+            if (hasSpaceInWaterTank(this.getWaterTank().getFluid().getAmount())) {
 
-                WATER_TIMER = WATER_TIMER + 1;
+                if (this.consumePowerForOperation() != null) {
 
-                if (WATER_TIMER > 10) {
+                    WATER_TIMER = WATER_TIMER + 1;
 
-                    ((IBucketPickupHandler) this.world.getBlockState(pos).getBlock()).pickupFluid(this.world, pos, this.world.getBlockState(pos));
-                    WATER_TIMER = 0;
+                    if (WATER_TIMER > 10) {
+
+                        ((IBucketPickupHandler) this.world.getBlockState(pos).getBlock()).pickupFluid(this.world, pos, this.world.getBlockState(pos));
+
+                        this.getWaterTank().fill(new FluidStack(Fluids.WATER,1000), IFluidHandler.FluidAction.EXECUTE);
+
+                        WATER_TIMER = 0;
+                    }
                 }
             }
         }
@@ -41,6 +63,29 @@ public class WaterPumpTileEntity extends AbstractMachineTileEntity {
 
     @Override
     protected void tickProcessing() {
+    }
+
+    public boolean hasSpaceInWaterTank(int water) {
+        return hasSpaceIn(water, this.getWaterTank().getFluid());
+    }
+
+    public boolean hasSpaceIn(int water, FluidStack storage) {
+        return water < TANK_CAPACITY;
+    }
+
+    @Override
+    public <T> LazyOptional<T> getCapabilityEnergy(Capability<T> capability, Direction facing) {
+        BlockState blockState = this.getBlockState();
+
+        if (blockState.hasProperty(WaterPump.FACING)) {
+            Direction blockDirection = blockState.get(WaterPump.FACING);
+
+            if (facing == null || facing == blockDirection) {
+                return super.getCapabilityEnergy(capability, facing);
+            }
+        }
+
+        return LazyOptional.empty();
     }
 
     @Override
@@ -63,6 +108,47 @@ public class WaterPumpTileEntity extends AbstractMachineTileEntity {
 
     public int getBasePowerForOperation() {
         return 1;
+    }
+
+    @Override
+    protected void createFluidHandlers(NamedComponentRegistry<IFluidHandler> registry) {
+        super.createFluidHandlers(registry);
+        this.waterTank = (FluidTank) registry.computeIfAbsent(this.getTankName(), k -> this.creatTank(k));
+    }
+
+    protected int getInitialTankCapacity(ResourceLocation name) {
+        return TANK_CAPACITY;
+    }
+
+    protected Predicate<FluidStack> getInitialTankValidator(ResourceLocation name) {
+        Fluid fluid = this.getTankFluid(name);
+        return fluid != null ? fs -> FluidUtil2.isEquivalentTo(fs, fluid) : null;
+    }
+
+    protected Fluid getTankFluid(ResourceLocation name) {
+        if (name.equals(this.getTankName())) {
+            return Fluids.WATER;
+        } else {
+            return null;
+        }
+    }
+
+    protected FluidTank creatTank(ResourceLocation name) {
+        return new FluidTank(this.getInitialTankCapacity(name), this.getInitialTankValidator(name)) {
+            @Override
+            protected void onContentsChanged() {
+                super.onContentsChanged();
+                WaterPumpTileEntity.this.markDirty();
+            }
+        };
+    }
+
+    public ResourceLocation getTankName() {
+        return WATER_TANK;
+    }
+
+    public FluidTank getWaterTank() {
+        return this.waterTank;
     }
 
     @Override
